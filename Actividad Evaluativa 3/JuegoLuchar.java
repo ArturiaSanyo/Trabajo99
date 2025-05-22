bca06932be4c497dc7f9203f6be9818e9e4d7ed5
ArturiaSanyo <126627811+ArturiaSanyo@users.noreply.github.com>
2025-05-21 21:33:26 -0500

    Create JuegoLuchar.java

---
+++ Actividad Evaluativa 3/JuegoLuchar.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class JuegoLucha {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Luchador jugador1 = new Luchador("Jugador 1");
+        Luchador jugador2 = new Luchador("Jugador 2");
+
+        System.out.println("¡Empieza el combate!");
+
+        while (jugador1.estaVivo() && jugador2.estaVivo()) {
+            System.out.println("\nTurno de " + jugador1.getNombre());
+            jugador1.atacar(jugador2);
+
+            if (!jugador2.estaVivo()) break;
+
+            System.out.println("\nTurno de " + jugador2.getNombre());
+            jugador2.atacar(jugador1);
+        }
+
+        if (jugador1.estaVivo()) {
+            System.out.println("\n" + jugador1.getNombre() + " ha ganado.");
+        } else {
+            System.out.println("\n" + jugador2.getNombre() + " ha ganado.");
+        }
+
+        scanner.close();
+    }
+}
+
+class Luchador {
+    private String nombre;
+    private int vida = 100;
+
+    public Luchador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean estaVivo() {
+        return vida > 0;
+    }
+
+    public void atacar(Luchador enemigo) {
+        int danio = (int)(Math.random() * 20 + 1); // Daño aleatorio entre 1 y 20
+        enemigo.recibirDanio(danio);
+        System.out.println(nombre + " ataca y causa " + danio + " de daño.");
+    }
+
+    public void recibirDanio(int cantidad) {
+        vida -= cantidad;
+        vida = Math.max(vida, 0);
+        System.out.println(nombre + " tiene " + vida + " de vida restante.");
+    }
+}
