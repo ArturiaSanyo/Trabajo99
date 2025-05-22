3769af9f144036f024b0a92e60454e3b1c39c5be
ArturiaSanyo <126627811+ArturiaSanyo@users.noreply.github.com>
2025-05-21 21:47:16 -0500

    Update JuegoLuchar.java

--- Actividad Evaluativa 3/JuegoLuchar.java
+++ Actividad Evaluativa 3/JuegoLuchar.java
@@ -5,10 +5,10 @@ public class JuegoLucha {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Luchador jugador1 = new Luchador("Jugador 1");
-        Luchador jugador2 = new Luchador("Jugador 2");
+        Espartano jugador1 = new Espartano("Espartano 1");
+        Espartano jugador2 = new Espartano("Espartano 2");
 
-        System.out.println("¡Empieza el combate!");
+        System.out.println("¡Empieza la batalla entre espartanos!");
 
         while (jugador1.estaVivo() && jugador2.estaVivo()) {
             System.out.println("\nTurno de " + jugador1.getNombre());
@@ -21,20 +21,20 @@ public class JuegoLucha {
         }
 
         if (jugador1.estaVivo()) {
-            System.out.println("\n" + jugador1.getNombre() + " ha ganado.");
+            System.out.println("\n" + jugador1.getNombre() + " ha ganado la batalla.");
         } else {
-            System.out.println("\n" + jugador2.getNombre() + " ha ganado.");
+            System.out.println("\n" + jugador2.getNombre() + " ha ganado la batalla.");
         }
 
         scanner.close();
     }
 }
 
-class Luchador {
+class Espartano {
     private String nombre;
     private int vida = 100;
 
-    public Luchador(String nombre) {
+    public Espartano(String nombre) {
         this.nombre = nombre;
     }
 
@@ -46,7 +46,7 @@ class Luchador {
         return vida > 0;
     }
 
-    public void atacar(Luchador enemigo) {
+    public void atacar(Espartano enemigo) {
         int danio = (int)(Math.random() * 20 + 1); // Daño aleatorio entre 1 y 20
         enemigo.recibirDanio(danio);
         System.out.println(nombre + " ataca y causa " + danio + " de daño.");
