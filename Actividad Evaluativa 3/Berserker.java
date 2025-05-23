c974ecc865c7c90b8947c5a175b21a6b9b3d56e6
ArturiaSanyo <126627811+ArturiaSanyo@users.noreply.github.com>
2025-05-23 10:25:03 -0500

    Update Berserker.java

--- Actividad Evaluativa 3/Berserker.java
+++ Actividad Evaluativa 3/Berserker.java
@@ -1,40 +1,18 @@
-public class Berserker {
-    private String nombre;
+public class Berserker extends Personaje {
     private int fuerza;
-    private int vida;
 
     public Berserker(String nombre, int fuerza) {
-        this.nombre = nombre;
+        super(nombre, 100);
         this.fuerza = fuerza;
-        this.vida = 100;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public boolean estaVivo() {
-        return vida > 0;
-    }
-
-    public void atacar(Espartano enemigo) {
-        int danio = fuerza + (int)(Math.random() * 10); // Daño basado en fuerza
+    public void atacar(Personaje enemigo) {
+        int danio = fuerza + random.nextInt(10); // fuerza base + 0 a 9
         enemigo.recibirDanio(danio);
         System.out.println(nombre + " ataca con furia causando " + danio + " de daño.");
     }
 
-    public void atacar(HechiceroArcano enemigo) {
-        int danio = fuerza + (int)(Math.random() * 10);
-        enemigo.recibirDanio(danio);
-        System.out.println(nombre + " golpea al hechicero causando " + danio + " de daño.");
-    }
-
-    public void recibirDanio(int cantidad) {
-        vida -= cantidad;
-        vida = Math.max(vida, 0);
-        System.out.println(nombre + " recibe " + cantidad + " de daño. Vida restante: " + vida);
-    }
-
+    @Override
     public void mostrarEstado() {
         System.out.println("Berserker: " + nombre);
         System.out.println("Fuerza: " + fuerza);
